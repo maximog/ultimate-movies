f08e7cdb43979fc4483d0c0dfd6a7e8a8a2fc4d4
maximog <maximo.gijon@gmail.com>
2020-05-22 16:28:40 -0300

    add lazy loading, Suspense

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar.component';
 import { StyledMain } from './App.styles';
-import DetailedView from './views/DetailedViews/DetailedView.component';
-import MovieView from './views/MovieView/MovieView.component';
-import HomeView from './views/HomeView/HomeView.component';
-import TvView from './views/TvView/TvView.component';
-import MediaView from './views/MediaView/MediaView.component';
-import PersonView from './views/PersonView/PersonView.component';
 
+const MovieView = lazy(() => import('./views/MovieView/MovieView.component'));
+const HomeView = lazy(() => import('./views/HomeView/HomeView.component'));
+const TvView = lazy(() => import('./views/TvView/TvView.component'));
+const MediaView = lazy(() => import('./views/MediaView/MediaView.component'));
+const PersonView = lazy(() => import('./views/PersonView/PersonView.component'));
+const DetailedView = lazy(() => import('./views/DetailedViews/DetailedView.component'));
 
 
 const App = () => {
@@ -17,14 +17,15 @@ const App = () => {
       <NavBar />
       <StyledMain>
         <Switch>
-          <Route exact path='/' component={HomeView} />
-          <Route exact path='/movie' component={MovieView} />
-          <Route exact path='/tv' component={TvView} />
-          <Route exact path='/:media/:type/page/:pageNumber' component={MediaView} />
-          <Route exact path='/:media/:genreName/:genreNumber/page/:pageNumber' component={MediaView} />
-          <Route exact path='/person/:id/:section' component={PersonView} />
-          <Route path='/:media/:id/:section/:seasonNumber?' component={DetailedView} />
-
+          <Suspense fallback={<div style={{color: 'white'}}>Loading....</div>}>
+            <Route exact path='/' component={HomeView} />
+            <Route exact path='/movie' component={MovieView} />
+            <Route exact path='/tv' component={TvView} />
+            <Route exact path='/:media/:type/page/:pageNumber' component={MediaView} />
+            <Route exact path='/:media/:genreName/:genreNumber/page/:pageNumber' component={MediaView} />
+            <Route exact path='/person/:id/:section' component={PersonView} />
+            <Route path='/:media/:id/:section/:seasonNumber?' component={DetailedView} />
+          </Suspense>
         </Switch>
       </StyledMain>
     </Router>
